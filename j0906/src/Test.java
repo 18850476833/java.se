4678cfbd7889071a3a6ddb55fdfe07d81936ce24
dong-jincheng <2664883574@qq.com>
2023-09-06 17:59:35 +0800

    j

--- j0906/src/Test.java
+++ j0906/src/Test.java
@@ -16,6 +16,29 @@ public class Test {
 
 
 
+    /*public static void main(String[] args) {
+        int[][] arr = {{11,33,55},{22,44,66,88},{131,214,315,146},{928,827,726,625},{424,525}};
+        int sum = add(arr);
+        System.out.println(sum);
+    }
+    public static int add(int[][] arr){
+        int sum = 0;
+        for (int[] anInt : arr) {
+            for (int add1 : anInt) {
+                sum = sum + add1;
+            }
+        }
+        return sum;*/
+        /*for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                sum = sum + arr[i][j];
+            }
+        }
+        return sum;*/
+    }
+
+
+
 
     /*public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);

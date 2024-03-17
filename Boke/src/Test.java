a5be4098141ded6201ee7cb0341fe134a291bba7
djc <2664883574@qq.com>
2024-03-17 13:49:56 +0800

    泛型博客完结

--- Boke/src/Test.java
+++ Boke/src/Test.java
@@ -6,16 +6,77 @@
  * Time: 8:25
  */
 class MyArray<T>{
-    public Object[] array = new Object[10];
+    public Object[] array1 = new Object[10];
     public void setvalue(int pos,T val){
-        array[pos] = val;
+        array1[pos] = val;
     }
     public T getvalue(int pos){
-        return (T)array[pos];
+        return (T)array1[pos];
     }
 }
+
+class Alg<T extends Comparable<T>> {
+    public T findMaxValue(T[] array) {
+        T max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (max.compareTo(array[i]) < 0) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+}
+
+class Alg2{
+    public <T extends Comparable<T>> T findMaxValue(T[] array){
+        T max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (max.compareTo(array[i]) < 0){
+                max = array[i];
+            }
+        }
+        return max;
+    }
+}
+
 public class Test {
     public static void main(String[] args) {
+        Alg2 alg2 = new Alg2();
+        Integer[] integers1 = {1,2,3,4,5,6,7};
+        Integer ret1 = alg2.findMaxValue(integers1);
+        System.out.println(ret1);
+    }
+
+
+
+
+
+
+
+
+
+    /*public static void main(String[] args) {
+        Alg<Integer> alg = new Alg<>();
+        Integer[] integers = {1, 2, 3, 4, 5, 6, 7};
+        Integer ret = alg.findMaxValue(integers);
+        System.out.println(ret);
+    }*/
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*public static void main(String[] args) {
         MyArray<Integer> myArray = new MyArray<Integer>();
         myArray.setvalue(0,10);
         myArray.setvalue(1,100);
@@ -29,7 +90,7 @@ public class Test {
         myArray1.setvalue(1,"ko");
         String b = myArray1.getvalue(0);
         System.out.println(b);
-
+*/
 
 
 
@@ -44,7 +105,6 @@ public class Test {
         Integer bb = 200;
         System.out.println(aa == bb);*/
 
-    }
 
 
 
@@ -86,7 +146,3 @@ public class Test {
     /*int i = 5;
     Integer b =i;
     */
-
-
-
-}

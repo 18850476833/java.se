f4753cabc3c09fadfdb88505418832e34db8b623
djc <2664883574@qq.com>
2024-04-07 23:12:44 +0800

    MySingleList完整版

--- SingleList/src/MySingle/MySingleList.java
+++ SingleList/src/MySingle/MySingleList.java
@@ -17,7 +17,7 @@ public class MySingleList implements IList{
     }
     public ListNode head;
 
-    public void createList(){
+    /*public void createList(){
         //穷举的方法写出一些元素
         ListNode node1 = new ListNode(12);
         ListNode node2 = new ListNode(23);
@@ -31,7 +31,7 @@ public class MySingleList implements IList{
         node4.next = node5;
 
         this.head = node1;
-    }
+    }*/
     @Override
     public void addFirst(int data) {
         ListNode node = new ListNode(data);
@@ -58,39 +58,126 @@ public class MySingleList implements IList{
     }
     @Override
     public void addIndex(int index, int data) {
-
-
-        
+        int len = size();
+        if (index < 0 || index > len){
+            return ; //自定义一个异常
+        }
+        if (index == 0){
+            addFirst(data);
+            return;
+        }
+        if (index == len){
+            addLast(data);
+            return;
+        }
+        ListNode cur = searchPrev(index);
+        ListNode node = new ListNode(data);
+        node.next = cur.next;
+        cur.next = node;
     }
+    private ListNode searchPrev(int index){
+        ListNode cur = head;
+        int cunt = 0;
+        while (cunt != index - 1){
+            cur = cur.next;
+            cunt++;
+        }
+        return cur;
+    }
+
 
     @Override
     public boolean contains(int key) {
+        ListNode cur = head;
+        while (cur != null){
+            if (cur.val == key){
+                return true;
+            }
+            cur = cur.next;
+        }
         return false;
     }
 
     @Override
     public void remove(int key) {
-
+        if (head == null){
+            return;//可以写一个异常
+        }
+        if (head.val == key){
+            head = head.next;
+            return;
+        }
+        ListNode cur = findPrev(key);
+        if (cur == null){
+            System.out.println("没有要删除的数字");
+            return;
+        }
+        ListNode del = cur.next;
+        cur.next = del.next;
     }
+    private ListNode findPrev(int key){
+        ListNode cur = head;
+        while (cur.next != null){
+            if (cur.next.val == key){
+                return cur;
+            }
+            cur = cur.next;
+        }
+        return null;
+    }
+
 
     @Override
     public void removeAllkey(int key) {
+        if (head == null){
+            return;
+        }
+        ListNode prev = head;
+        ListNode cur = head.next;
+        while (cur != null){
+            if (cur.val == key){
+                prev.next = cur.next;
+                cur = cur.next;
 
+            }else {
+                prev = cur;
+                cur = cur.next;
+            }
+        }
+        if (head.val == key){
+            head = head.next;
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        int count = 0;
+        ListNode cur = head;
+        while (cur != null){
+            count++;
+            cur = cur.next;
+        }
+        return count;
     }
 
     @Override
     public void clear() {
-
+        ListNode cur = head;
+        while (cur != null){
+            ListNode curNext = cur.next;
+            cur.next = null;
+            cur = curNext;
+        }
+        head = null;
     }
 
     @Override
     public void display() {
-
+        ListNode cur = head;
+        while (cur != null){
+            System.out.print(cur.val + " ");
+            cur = cur.next;
+        }
+        System.out.println( );
     }
-
 }
